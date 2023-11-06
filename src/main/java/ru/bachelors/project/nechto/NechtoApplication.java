ad2852c9946b5738f493d252a1444a6d600c525f
Alexander-1703 <gladyshev.aa@edu.spbstu.ru>
2023-11-07 00:04:23 +0300

    initial commit

---
+++ src/main/java/ru/bachelors/project/nechto/NechtoApplication.java
@@ -0,0 +1,13 @@
+package ru.bachelors.project.nechto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NechtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NechtoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bachelors/project/nechto/NechtoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bachelors.project.nechto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NechtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

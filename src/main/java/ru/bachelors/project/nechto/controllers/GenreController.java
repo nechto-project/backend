a5b36244300eed156b054b6a4d08643b81ca671a
radkoff26 <radko.slava@mail.ru>
2023-11-27 10:47:50 +0300

    add endpoint with most popular genres

---
+++ src/main/java/ru/bachelors/project/nechto/controllers/GenreController.java
@@ -0,0 +1,27 @@
+package ru.bachelors.project.nechto.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.bachelors.project.nechto.models.Genre;
+import ru.bachelors.project.nechto.service.GenreService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/genre")
+public class GenreController {
+
+    private final GenreService genreService;
+
+    @Autowired
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
+
+    @GetMapping
+    public List<Genre> getAllGenres() {
+        return genreService.findAllGenres();
+    }
+}
--- src/main/java/ru/bachelors/project/nechto/service/GenreService.java
+++ src/main/java/ru/bachelors/project/nechto/service/GenreService.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 import ru.bachelors.project.nechto.models.Genre;
 import ru.bachelors.project.nechto.repositories.GenreRepository;
 
+import java.util.Collections;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -14,12 +17,53 @@ public class GenreService {
     private final GenreRepository genreRepository;
 
     public void createGenre(Genre genre) {
-        if(genreRepository.findByName(genre.getName()) != null) {
+        if (genreRepository.findByName(genre.getName()) != null) {
             log.info("Genre already exist: {}", genre);
             return;
         }
         log.info("Create genre: {}", genre);
         genreRepository.save(genre);
+    }
+
+    public List<Genre> findAllGenres() {
+        return List.of(
+                new Genre(
+                        1L, "аниме", Collections.emptyList()
+                ),
+                new Genre(
+                        6L, "детектив", Collections.emptyList()
+                ),
+                new Genre(
+                        7L, "детский", Collections.emptyList()
+                ),
+                new Genre(
+                        10L, "драма", Collections.emptyList()
+                ),
+                new Genre(
+                        13L, "комедия", Collections.emptyList()
+                ),
+                new Genre(
+                        17L, "мелодрама", Collections.emptyList()
+                ),
+                new Genre(
+                        22L, "приключения", Collections.emptyList()
+                ),
+                new Genre(
+                        24L, "семейный", Collections.emptyList()
+                ),
+                new Genre(
+                        25L, "спорт", Collections.emptyList()
+                ),
+                new Genre(
+                        27L, "триллер", Collections.emptyList()
+                ),
+                new Genre(
+                        28L, "ужасы", Collections.emptyList()
+                ),
+                new Genre(
+                        29L, "фантастика", Collections.emptyList()
+                )
+        );
     }
 
     public Genre findGenreByName(String name) {
